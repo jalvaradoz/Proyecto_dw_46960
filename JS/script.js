fbc4ba9647270cf28f7c4d51b05631750d3d368b
jalvaradoz <alvarado_joey@outlook.com>
2023-11-06 21:25:14 -0600

    animaciones varias

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const overlay = document.querySelector(".overlay");
+    const logo = document.querySelector(".logo");
+
+    // Agregamos un evento para detectar el final de la animación del logo
+    logo.addEventListener("animationend", function() {
+        // Iniciamos la animación de desvanecimiento de la superposición
+        overlay.style.animation = "fade-out-overlay 1s forwards";
+    });
+});
